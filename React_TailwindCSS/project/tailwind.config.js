2b4815266f8ed1ca453985d67314a917e095fca4
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-31 21:07:26 +0700

    Update tailwind.config.js

--- React_TailwindCSS/project/tailwind.config.js
+++ React_TailwindCSS/project/tailwind.config.js
@@ -1,16 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors:{
-        brightColor: "#F4511F",
-        backgroundColor: "#b7bca9",
-        lightText: "#959595",
-      },
-    },
-  },
-  plugins: [],
-}
+
